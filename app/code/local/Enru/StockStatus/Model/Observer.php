1518d6f16813ad837e6b0ede7e0b0839bec9708f
Neill Russell <n@enru.co.uk>
2015-04-30 16:16:56 +0100

    added extension code

---
+++ app/code/local/Enru/StockStatus/Model/Observer.php
@@ -0,0 +1,40 @@
+<?php
+
+class Enru_StockStatus_Model_Observer {
+
+	function setStockStatus($observer) {
+
+		$product = $observer->getProduct();
+
+		$stockData = $product->getStockData();
+
+		if(!is_array($stockData) || !isset($stockData['qty'])) {
+			$stockData = array(
+				'qty' => intval(Mage::getModel('cataloginventory/stock_item')->loadByProduct($product)->getQty()),
+			);
+		}
+
+		$qty = $stockData['qty'];
+
+		// if it's a configurable product
+		if($product->getTypeId() == 'configurable' && $product->getId()) {
+			// tally up qty of assoc products
+			$qty=0;
+			$data = $product->getConfigurableProductsData();
+			if (is_array($data)) {
+				$productIds = array_keys($data);
+				foreach($productIds as $prodId){
+					$sprod=Mage::getModel("catalog/product")->load($prodId);
+					$sqty = intval(Mage::getModel('cataloginventory/stock_item')->loadByProduct($sprod)->getQty());
+					$qty += $sqty; 
+				}   
+			} 
+		}
+
+		// update the stock to in or out of stock depending on current qty
+		$stockData['is_in_stock'] =  $qty ? 1 : 0;      
+		Mage::log($product->getId() . " " . $stockData['is_in_stock']. "=" .$qty);
+		$product->setStockData($stockData);
+	}
+
+}
