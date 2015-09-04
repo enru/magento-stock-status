0135ddde84e714d5ef9f104b1399af184229901a
Neill Russell <git@enru.co.uk>
2015-09-04 16:39:26 +0100

    Update Observer.php
    using both methods

--- app/code/local/Enru/StockStatus/Model/Observer.php
+++ app/code/local/Enru/StockStatus/Model/Observer.php
@@ -20,13 +20,24 @@ class Enru_StockStatus_Model_Observer {
 		if($product->getData('type_id') == 'configurable' && $product->getId()) {
 			// tally up qty of assoc products
 			$qty=0;
-                        $childIds = Mage::getModel('catalog/product_type_configurable')->getChildrenIds($product->getId());
-                        if(is_array($childIds) && is_array($childIds[0])) {
-                                foreach($childIds[0] as $prodId){
-                                        $sprod=Mage::getModel("catalog/product")->load($prodId);
-                                        $sqty = intval(Mage::getModel('cataloginventory/stock_item')->loadByProduct($sprod)->getQty());
-                                        $qty += $sqty;
+                        $productIds = array();
+
+                        $data = $product->getConfigurableProductsData();
+
+                        if (is_array($data)) {
+                                $productIds = array_keys($data);
+                        }
+                        else {
+                                $childIds = Mage::getModel('catalog/product_type_configurable')->getChildrenIds($product->getId());
+                                if(is_array($childIds) && is_array($childIds[0])) {
+                                        $productIds = $childIds[0];
                                 }
+                        }
+
+                        foreach($productIds as $prodId){
+                                $sprod=Mage::getModel("catalog/product")->load($prodId);
+                                $sqty = intval(Mage::getModel('cataloginventory/stock_item')->loadByProduct($sprod)->getQty());
+                                $qty += $sqty;
                         }
 		}
 
